0316f7db9e30fd0699adfe5d99f91702f7e22559
Mason Ellwood <masonellwood@gmail.com>
2020-05-20 22:44:31 -0700

    test model and provider data added

---
+++ lib/models/gmg_recipes_client.dart
@@ -0,0 +1,27 @@
+class GMGRecipesClient {
+  final arr = ["Beef", "Poultry", "Seafood", "Pizza", "Lamb", "Dessert", "Pork", "Veggies", "Cocktails", "Appetizers", "Baking", "Breakfast"];
+
+  String content;
+  String title;
+  String postName;
+  String website;
+  String prepTime;
+  String idleTime;
+  String cookTime;
+  String pellets;
+  String serves;
+  String mainImage;
+
+  GMGRecipesClient({
+    this.content,
+    this.title,
+    this.postName,
+    this.website,
+    this.prepTime,
+    this.idleTime,
+    this.cookTime,
+    this.pellets,
+    this.serves,
+    this.mainImage,
+  });
+}
